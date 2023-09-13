7cc731f2b02445c45c8402bddf16a25893ab067d
Zendard <thijsmuylaert@hotmail.com>
2023-09-13 18:29:03 +0200

    Re-add basic routing

--- index.ts
+++ index.ts
@@ -3,8 +3,23 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-	res.send("Hello World!");
+	res.render("index");
+});
+app.get("/zok", (req, res) => {
+	res.render("zok");
+});
+app.get("/onze-kines", (req, res) => {
+	res.render("onze-kines");
+});
+app.get("/contact", (req, res) => {
+	res.render("contact");
+});
+app.get("/lid-worden", (req, res) => {
+	res.render("lid-worden");
 });
 
 app.listen(port, () => {
