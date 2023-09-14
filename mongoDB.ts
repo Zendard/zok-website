1208194eb97745b07869ea3797580de2ea02228e
Zendard <thijsmuylaert@hotmail.com>
2023-09-14 22:39:10 +0200

    Add interactive map to kalender item page

--- mongoDB.ts
+++ mongoDB.ts
@@ -1,6 +1,7 @@
 import { MongoClient } from "mongodb";
 import { uri } from "./uri";
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, Response, application } from "express";
+import Nominatim from "nominatim-ts";
 
 async function getDataEvent(req: Request, res: Response, next: NextFunction) {
 	const eventName = req.params.event;
@@ -10,10 +11,14 @@ async function getDataEvent(req: Request, res: Response, next: NextFunction) {
 			.db("Zok")
 			.collection("Calendar")
 			.findOne({ name: eventName });
+		const location = await Nominatim.search({ q: event.location });
 		res.locals.event = (await event) || {
 			title: "Bericht niet gevonden",
 			date: "",
 		};
+		console.log(location);
+		res.locals.event.lat = parseFloat(location[0].lat);
+		res.locals.event.lon = parseFloat(location[0].lon);
 	} finally {
 		await client.close();
 		await next();
@@ -28,9 +33,10 @@ async function getDataAll(req: Request, res: Response, next: NextFunction) {
 			.collection("Calendar")
 			.find()
 			.toArray();
-		events.forEach((event) => {
+		events.forEach(async (event) => {
 			const date = new Date(event.date);
-			event.date = `${date.getDay()}/${date.getMonth()}/${date.getFullYear()}`;
+			event.date =
+				await `${date.getDay()}/${date.getMonth()}/${date.getFullYear()}`;
 		});
 		res.locals.events = events;
 	} finally {
