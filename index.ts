833a0ca6b4664f3d97c279b7cb231587a473e46f
Zendard <72878733+Zendard@users.noreply.github.com>
2023-09-16 15:51:22 +0200

    Revert "Update production branch"

--- index.ts
+++ index.ts
@@ -3,13 +3,49 @@ import { getDataAll } from "./mongoDB";
 if (!Bun.env.MONGODB_URI) {
 	console.error("Please set uri ENV");
 }
+import compression from "compression";
+import helmet from "helmet";
+import { rateLimit } from "express-rate-limit";
 
 const app = express();
-const port = parseInt(Bun.env.PORT || "3000");
+const port = Bun.env.PORT;
+const allowedSources = [
+	"'self'",
+	"kit.fontawesome.com",
+	"unpkg.com",
+	"ka-f.fontawesome.com",
+	"zuid-oost-vlaamsekinekring.be",
+	"fonts.gstatic.com",
+	"window.location.href='/kalender/kmo'",
+	"'unsafe-inline'",
+	"a.tile.openstreetmap.fr",
+	"b.tile.openstreetmap.fr",
+	"c.tile.openstreetmap.fr",
+];
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
-
+app.use(compression());
+app.use(
+	helmet({
+		contentSecurityPolicy: {
+			directives: {
+				"script-src": allowedSources,
+				"default-src": allowedSources,
+				"img-src": allowedSources,
+				"font-src": allowedSources,
+				"script-src-attr": allowedSources,
+			},
+		},
+	})
+);
+const limiter = rateLimit({
+	windowMs: 1 * 60 * 1000, // 1 minute
+	max: 20,
+	standardHeaders: "draft-7",
+	legacyHeaders: false,
+});
+app.use(limiter);
 
 app.get("/", getDataAll, (req, res) => {
 	res.render("index");
