e5ea539d470990d345ebb2da3f7ac414cdffc087
Zendard <thijsmuylaert@hotmail.com>
2023-10-31 16:15:46 +0100

    Improve security with helmet

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import kalenderRouter from './routes/kalender';
 import adminRouter from './routes/admin';
 import {getKalender, getBerichten} from './databaseFetch';
+import compression from 'compression';
+import helmet from 'helmet';
 
 if (!Bun.env.MONGODB_URI) {
 	throw new Error('Please set mongo uri');
@@ -9,9 +11,18 @@ if (!Bun.env.MONGODB_URI) {
 
 const app = express();
 app.set('view engine', 'ejs');
-app.set('views', './views')
+app.set('views', './views');
 app.use(express.static('./public'));
+app.use(compression());
 const port = Bun.env.PORT||'3000';
+app.use(
+	helmet.contentSecurityPolicy({
+		directives: {
+			'script-src': ['\'self\'', 'http://*.fontawesome.com'],
+			'default-src': ['\'self\'', 'http://*.fontawesome.com']
+		},
+	}),
+);
 
 app.get('/', async (req, res) => {
 	res.render('index',{kalender:await getKalender(),berichten:await getBerichten()});
