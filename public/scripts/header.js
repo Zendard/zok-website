5ccbcb4315cf3f59e101adf3463a6260093b14ad
Zendard <thijsmuylaert@hotmail.com>
2023-11-05 13:09:24 +0100

    Fix mobile nav

--- public/scripts/header.js
+++ public/scripts/header.js
@@ -2,7 +2,5 @@ const nav = document.querySelector('nav');
 const hamburgerButton = document.getElementById('nav-button');
 
 hamburgerButton.addEventListener('click', (e) => {
-	hamburgerButton.style.display = 'none';
 	nav.classList.toggle('nav-open');
-	setTimeout(() => { hamburgerButton.style.display = 'initial'; }, 1000);
 });
