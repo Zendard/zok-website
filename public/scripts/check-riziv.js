1b03ba78a19a26ed8ad1fb78b9b19e4fed0a9eec
Zendard <thijsmuylaert@hotmail.com>
2023-11-15 18:00:59 +0100

    Update riziv input

--- public/scripts/check-riziv.js
+++ public/scripts/check-riziv.js
@@ -1,9 +1,13 @@
-const rizivInput=document.querySelector('input[name=riziv]');
+const rizivInput = document.querySelector('input[name=riziv]');
 
-rizivInput.addEventListener('input',(e)=>{
-	if(97-((5+rizivInput.value.split('-')[0])%97)!=rizivInput.value.split('-')[1]){
-		rizivInput.style.borderColor='red';
-	}else{
-		rizivInput.style.borderColor='green';
+rizivInput.addEventListener('input', (e) => {
+	if (rizivInput.value.length == 5 && e.inputType == 'insertText') {
+		rizivInput.value = rizivInput.value + '-';
+	}
+
+	if (97 - ((5 + rizivInput.value.split('-')[0]) % 97) != rizivInput.value.split('-')[1]) {
+		rizivInput.style.borderColor = 'red';
+	} else {
+		rizivInput.style.borderColor = 'green';
 	}
 }); 
