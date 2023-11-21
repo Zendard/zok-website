2c6da35e7bf902eccb5e8c84613de83c18f78087
Zendard <thijsmuylaert@hotmail.com>
2023-11-21 09:19:32 +0100

    Fix stylesheet link strings

--- sendMail.ts
+++ sendMail.ts
@@ -2,26 +2,26 @@ import nodemailer from 'nodemailer';
 import { Address } from 'nodemailer/lib/mailer';
 
 if(!Bun.env.EMAIL_NAME||!Bun.env.EMAIL_PASSWD||!Bun.env.EMAIL_TO||!Bun.env.CLIENT_ID||!Bun.env.CLIENT_SECRET||!Bun.env.REFRESH_TOKEN||!Bun.env.ACCES_TOKEN){
-	throw new Error('Please set email name, passwd, client_id, client_secret, refresh token, to and/or refresh token env');
+	console.log('Please set email name, passwd, client_id, client_secret, refresh token, to and/or refresh token env');
 }
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
+	host: 'smtp.gmail.com',
+	port: 465,
+	secure: true,
 	debug: true,
 	tls: {
-        // do not fail on invalid certs
-        rejectUnauthorized: false
-    },
-    auth: {
-        type: 'OAuth2',
-        user: Bun.env.EMAIL_NAME,
-        clientId: Bun.env.CLIENT_ID,
-        clientSecret: Bun.env.CLIENT_SECRET,
-        refreshToken: Bun.env.REFRESH_TOKEN,
-        accessToken: Bun.env.ACCES_TOKEN
-    }
+		// do not fail on invalid certs
+		rejectUnauthorized: false
+	},
+	auth: {
+		type: 'OAuth2',
+		user: Bun.env.EMAIL_NAME,
+		clientId: Bun.env.CLIENT_ID,
+		clientSecret: Bun.env.CLIENT_SECRET,
+		refreshToken: Bun.env.REFRESH_TOKEN,
+		accessToken: Bun.env.ACCES_TOKEN
+	}
 });
 
 const sendMail = (name:string|Address|undefined, subject:string, text:string, cb:any) => {
@@ -34,6 +34,6 @@ const sendMail = (name:string|Address|undefined, subject:string, text:string, cb
 	};
 
 	transporter.sendMail(mailOptions);
-	transporter.close()};
+	transporter.close();};
 
 export default sendMail;
