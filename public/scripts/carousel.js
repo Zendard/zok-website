23c07d31b812501de8f13c7bf5cfe0bf1a8655e9
Zendard <thijsmuylaert@hotmail.com>
2023-12-12 13:58:14 +0100

    Replace bestuur img on index with slideshow

---
+++ public/scripts/carousel.js
@@ -0,0 +1,20 @@
+const slideshow = document.querySelector('.carousel');
+const slides = slideshow.querySelectorAll('.carousel>img');
+let currentSlide = 0;
+
+function showSlide(index) {
+	slides.forEach((slide, i) => {
+		if (i === index) {
+			slide.classList.add('active');
+		} else {
+			slide.classList.remove('active');
+		}
+	});
+}
+
+function nextSlide() {
+	currentSlide = (currentSlide + 1) % slides.length;
+	showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 5000);
