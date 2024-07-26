a8804ca9a6e65cd7a4ea7c5815c93800a65329a6
Zendard <thijsmuylaert@hotmail.com>
2024-07-26 16:13:24 +0200

    Rewrite wie_zijn_wij page

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,11 @@ async fn index() -> Template {
     Template::render("index", context! {})
 }
 
+#[get("/wie-zijn-wij")]
+async fn wie_zijn_wij() -> Option<NamedFile> {
+    NamedFile::open("templates/wie_zijn_wij.html").await.ok()
+}
+
 #[catch(404)]
 async fn not_found() -> Option<NamedFile> {
     NamedFile::open("templates/404.html").await.ok()
@@ -17,7 +22,7 @@ async fn not_found() -> Option<NamedFile> {
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![index])
+        .mount("/", routes![index, wie_zijn_wij])
         .register("/", catchers![not_found])
         .mount("/", FileServer::from("public"))
         .attach(Template::fairing())
