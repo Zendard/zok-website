a19fcbd7d8da4b3d2e63597793f4db126165f78a
Zendard <thijsmuylaert@hotmail.com>
2024-07-26 16:10:38 +0200

    Rewrite 404 page

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
-use rocket::fs::FileServer;
+use rocket::fs::{FileServer, NamedFile};
 use rocket_dyn_templates::{context, Template};
 
 #[macro_use]
 extern crate rocket;
 
 #[get("/")]
-fn index() -> Template {
+async fn index() -> Template {
     Template::render("index", context! {})
+}
+
+#[catch(404)]
+async fn not_found() -> Option<NamedFile> {
+    NamedFile::open("templates/404.html").await.ok()
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .mount("/", routes![index])
+        .register("/", catchers![not_found])
         .mount("/", FileServer::from("public"))
         .attach(Template::fairing())
 }
