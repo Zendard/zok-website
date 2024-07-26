87c51aff14a85b471777d53605d37025c0c134b0
Zendard <thijsmuylaert@hotmail.com>
2024-07-26 15:19:41 +0200

    Clean repo

--- public/kalender/scripts/header.js
+++
@@ -1,8 +0,0 @@
-const nav = document.querySelector('nav');
-const hamburgerButton = document.getElementById('nav-button');
-
-hamburgerButton.addEventListener('click', (e) => {
-	hamburgerButton.style.display = 'none';
-	nav.classList.toggle('nav-open');
-	setTimeout(() => { hamburgerButton.style.display = 'initial'; }, 1000);
-});
--- public/scripts/header.js
+++
@@ -1,6 +0,0 @@
-const nav = document.querySelector('nav');
-const hamburgerButton = document.getElementById('nav-button');
-
-hamburgerButton.addEventListener('click', (e) => {
-	nav.classList.toggle('nav-open');
-});
