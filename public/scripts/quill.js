dd86af97a023bdfe81d2329f28b1e398b32fa665
Zendard <thijsmuylaert@hotmail.com>
2024-11-08 11:49:17 +0100

    Fix description when adding event

--- public/scripts/quill.js
+++ public/scripts/quill.js
@@ -1,3 +1,13 @@
 const quill = new Quill("#editor", {
   theme: "snow"
 })
+
+const form = document.querySelector('form');
+form.addEventListener('formdata', (event) => {
+  // Append Quill content before submitting
+  event.formData.append('description', quill.getSemanticHTML());
+});
+
+document.getElementById('test').addEventListener('click', ()=>{
+  console.log(quill.getSemanticHTML())
+})
