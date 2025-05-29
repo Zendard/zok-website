b3921c77ab47bc30522ead5aa627adf02eedab15
Zendard <thijsmuylaert@hotmail.com>
2025-05-29 13:06:10 +0200

    Add start time chip

--- public/scripts/get_items.js
+++ public/scripts/get_items.js
@@ -18,34 +18,56 @@ function add_events(events_json) {
 }
 
 function make_event(event) {
+  // Parent anchor
   const anchor = document.createElement("a")
   anchor.href = `/kalender/${event.id}`
 
+  // Parent div
   const div = document.createElement("div")
   div.classList.add("event")
 
+  // Header
   const header = document.createElement("div")
   header.classList.add("event-header")
 
+  // Title
   const title = document.createElement("h3")
   title.innerText = event.title
 
+  // Image
   const img = document.createElement("img")
   img.src = event.img_path
 
+  // Chips list
   const chips = document.createElement("div")
   chips.classList.add("chips")
 
+  // Date chip
   const date = document.createElement("h3")
   date.classList.add("chip")
   date.innerText = " " + event.date
 
+  // Date chip symbol
   const date_symbol = document.createElement("i")
   date_symbol.classList.add("fa-solid")
   date_symbol.classList.add("fa-calendar-days")
 
+  // Time chip
+  const time = document.createElement("h3")
+  time.classList.add("chip")
+  time.innerText = " " + event.start
+
+  // Time chip symbol
+  const time_symbol = document.createElement("i")
+  time_symbol.classList.add("fa-solid")
+  time_symbol.classList.add("fa-clock")
+
+
+
   date.prepend(date_symbol)
   chips.appendChild(date)
+  time.prepend(time_symbol)
+  chips.appendChild(time)
   header.appendChild(img)
   header.appendChild(chips)
   div.appendChild(header)
